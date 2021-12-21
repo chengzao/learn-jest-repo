0091ed5147869cfa6efdb70077cdfe3c7b5c9f64
chengzao <czhlink@163.com>
2021-12-21 16:41:57 +0800

    feat: 添加elementui测试组件

---
+++ packages/vue2/tests/unit/Button.spec.js
@@ -0,0 +1,17 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Button from '@/components/Button.vue'
+import ElementUI from 'element-ui' 
+
+const localVue = createLocalVue() 
+localVue.use(ElementUI)
+
+describe('shallowMount HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(Button, {
+      propsData: { msg },
+      localVue
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
